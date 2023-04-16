0bf67ea759a0b7a4e6a031df9e49dbca04d8612f
SalmaAhmed20 <salma.ahmed.anees@stud.cu.eg>
2023-04-16 21:20:00 +0200

    lab 2

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,14 +1,17 @@
-resource "aws_key_pair" "tf-key-pair" {
-  key_name   = "tf-key-pair"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
+# generate public and private key pair 
+resource "tls_private_key" "rsa-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+# create aws key pair by public key
+resource "aws_key_pair" "tf-key-pair" {
+  key_name   = "tf-key-pair"
+  public_key = tls_private_key.rsa-key.public_key_openssh
+}
+# store private key pair into my machine to use it with ssh connect 
 resource "local_file" "tf-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "tf-key-pair"
+  content  = tls_private_key.rsa-key.private_key_pem
+  filename = "tf-key-pair.pem"
 }
 resource "aws_instance" "bastion" {
   ami                         = var.AMI
@@ -17,11 +20,17 @@ resource "aws_instance" "bastion" {
   subnet_id                   = module.networks.PubsubnetID1
   associate_public_ip_address = true
   vpc_security_group_ids      = [module.networks.sgID1]
+  user_data                   = <<-EOF
+              #!/bin/bash
+                echo '${tls_private_key.rsa-key.private_key_pem}' > /home/ubuntu/tf-key-pair.pem
+                chmod 400 tf-key-pair.pem
+             EOF            
+
   tags = {
     Name = "bastion"
   }
   provisioner "local-exec" {
-    command = "echo ${self.public_ip}"
+    command = "echo ${self.public_ip} > inventory"
   }
 }
 resource "aws_network_interface" "privatenet" {
@@ -35,11 +44,13 @@ resource "aws_instance" "application" {
   ami           = var.AMI
   instance_type = var.INSTANCE_TYPE
   key_name      = "tf-key-pair"
-  depends_on = [module.networks.natt]
-  user_data = <<EOF
-  #!/bin/bash
-  sudo apt-get update -y && sudo apt install mysql-client -y
-  EOF
+  depends_on    = [module.networks.natt]
+  user_data                   = <<-EOF
+              #!/bin/bash
+                echo '${tls_private_key.rsa-key.private_key_pem}' > /home/ubuntu/tf-key-pair.pem
+                chmod 400 tf-key-pair.pem
+                apt install mysql-client -y
+             EOF            
   network_interface {
     network_interface_id = aws_network_interface.privatenet.id
     device_index         = 0
